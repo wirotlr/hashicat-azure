4325c1f46885532aad25d98af1f7a4d354627253
jurig <wiroot92@gmail.com>
2023-09-08 07:25:42 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,7 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "West Europe"
+}
+
 module "network" {
   source  = "app.terraform.io/terraform-mencret-azure/network/azurerm"
   version = "3.5.0"
-  resource_group_name = azurerm_resource_group
+  resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
